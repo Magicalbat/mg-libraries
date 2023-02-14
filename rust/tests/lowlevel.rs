9217b53ad959ed4d137478e5db5c25b0b0cc41b7
BD103 <59022059+BD103@users.noreply.github.com>
2023-02-14 12:30:48 -0500

    feat: lowlevel example

---
+++ rust/tests/lowlevel.rs
@@ -0,0 +1,22 @@
+use mga::{self, lowlevel::UnsafeArena};
+
+fn main() {
+    unsafe {
+        let mut arena = UnsafeArena::new(mga::mib_to_bytes(1));
+
+        let nums = arena.alloc_zeroed_slice::<u64>(64);
+
+        for i in 0..(*nums).len() {
+            (*nums)[i] = i as u64;
+        }
+
+        assert_eq!((*nums)[0], 0);
+        assert_eq!((*nums)[3], 3);
+        assert_eq!((*nums)[63], 63);
+
+        // De-allocate nums, make sure not to use the it after this!
+        arena.reset();
+
+        // Arena automatically gets freed here.
+    }
+}
