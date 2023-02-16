bcc8c0015dd929a22c2e8348c7d5c3922e68c7bf
BD103 <59022059+BD103@users.noreply.github.com>
2023-02-16 10:11:04 -0500

    feat: malloc backend support

--- rust/mga-sys/build.rs
+++ rust/mga-sys/build.rs
@@ -2,5 +2,11 @@ fn main() {
     println!("cargo:rerun-if-changed=../../mg_arena.h");
     println!("cargo:rerun-if-changed=mg_arena_impl.c");
 
-    cc::Build::new().file("mg_arena_impl.c").compile("mg_arena");
+    let mut build = cc::Build::new();
+
+    // #define MGA_FORCE_MALLOC
+    #[cfg(feature = "malloc")]
+    build.define("MGA_FORCE_MALLOC", None);
+
+    build.file("mg_arena_impl.c").compile("mg_arena");
 }
